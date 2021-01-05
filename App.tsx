8e4014c8dfb6122d033c16b258fe033d05d66265
Danil <diamondrock17rocker@gmail.com>
2021-01-05 17:08:56 +0200

    Add Lottie animation

--- App.tsx
+++ App.tsx
@@ -2,17 +2,33 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
 import SplashScreen from 'react-native-splash-screen';
-import {MainNavigation} from './src/navigation/MainNavigation';
+import {MainNavigation} from '@navigation/MainNavigation';
+import {Lottie} from '@components/lottie_animation';
+
+const LOADING_TIME = 5000;
 
 export class App extends React.Component {
-  public componentDidMount() {
-    SplashScreen.hide();
+  public state = {
+    isLoading: false,
+  };
+
+  public async componentDidMount() {
+    await SplashScreen.hide();
+    await setTimeout(() => {
+      this.setState({isLoading: true});
+    }, LOADING_TIME);
   }
   public render() {
     return (
-      <NavigationContainer>
-        <MainNavigation />
-      </NavigationContainer>
+      <>
+        {this.state.isLoading ? (
+          <NavigationContainer>
+            <MainNavigation />
+          </NavigationContainer>
+        ) : (
+          <Lottie />
+        )}
+      </>
     );
   }
 }
